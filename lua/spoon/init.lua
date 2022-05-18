e3e5b32c38ad2def8079f900750240a5b56446ba
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-18 18:08:32 +0430

    Stop using lua continue hack
    Since it broke stylua formatting

--- lua/spoon/init.lua
+++ lua/spoon/init.lua
@@ -7,22 +7,21 @@ local snippets = require("spoon.snippets")
 
 M.setup = function(opts)
   -- Merges the given opts with default opts (prefers theirs)
-  if opts == nil then opts = {} end
+  if opts == nil then
+    opts = {}
+  end
+
   for key, value in pairs(defaults.opts) do
-    if opts[key] ~= nil then goto continue end
-
-    opts[key] = value
-
-    ::continue::
+    if opts[key] == nil then
+      opts[key] = value
+    end
   end
 
   -- Adds enabled languages' snippets
   for lang, enable in pairs(opts.langs) do
-    if not enable then goto continue end
-
-    ls.add_snippets(lang, snippets[lang])
-
-    ::continue::
+    if enable then
+      ls.add_snippets(lang, snippets[lang])
+    end
   end
 end
 
