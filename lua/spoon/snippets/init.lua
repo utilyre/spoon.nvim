24e275c6dd065fd4367c9327af852cb20270fe83
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-18 18:05:29 +0430

    Implement lua snippets with a single snippet

---
+++ lua/spoon/snippets/init.lua
@@ -0,0 +1,5 @@
+local lua = require("spoon.snippets.lua")
+
+return {
+  lua = lua,
+}
---
+++ lua/spoon/snippets/lua.lua
@@ -0,0 +1,29 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local fmt = require("luasnip.extras.fmt").fmt
+local m = require("luasnip.extras").m
+local lambda = require("luasnip.extras").l
+
+return {
+  s(
+    "req",
+    fmt([[local {} = require("{}")]], {
+      f(function(args)
+        local module = args[1][1] or ""
+        local parts = vim.split(module, ".", { plain = true })
+        return parts[#parts]
+      end, { 1 }),
+      i(1, "module"),
+    })
+  ),
+}
