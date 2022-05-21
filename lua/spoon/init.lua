31cb20fdcd622cb6426935eaec289ef2fdef3621
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-21 14:03:33 +0430

    Enable snippets to access cfg via vim.g.spoon_config

--- lua/spoon/init.lua
+++ lua/spoon/init.lua
@@ -3,7 +3,6 @@ local M = {}
 local ls = require("luasnip")
 
 local defaults = require("spoon.defaults")
-local snippets = require("spoon.snippets")
 
 M.setup = function(opts)
   if opts == nil then
@@ -11,14 +10,19 @@ M.setup = function(opts)
   end
 
   -- Merges the given opts with default opts (prefers theirs)
+  local cfg = opts
   for key, value in pairs(defaults.opts) do
-    if opts[key] == nil then
-      opts[key] = value
+    if cfg[key] == nil then
+      cfg[key] = value
     end
   end
 
+  -- Make cfg global so it will be accessible in snippets
+  vim.g.spoon_config = cfg
+  local snippets = require("spoon.snippets")
+
   -- Adds enabled languages' snippets
-  for lang, enable in pairs(opts.langs) do
+  for lang, enable in pairs(cfg.langs) do
     if enable then
       ls.add_snippets(lang, snippets[lang])
     end
