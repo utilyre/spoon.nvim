28d56fdb67e0faaa3d1409639a595c955f74c8c3
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-22 12:57:08 +0430

    Make the concept of higher order snippets
    Has these two hos for now:
    - time
    - same

--- lua/spoon/hos.lua
+++ lua/spoon/hos.lua
@@ -14,11 +14,26 @@ local fmt = require("luasnip.extras.fmt").fmt
 local m = require("luasnip.extras").m
 local lambda = require("luasnip.extras").l
 
-local hos = require("spoon.hos")
+local M = {}
 
-return {
-  s(
-    "time",
-    hos.time("%m/%d/%G %H:%M")
-  ),
-}
+---Has the same text as the given node
+---@param node table
+---@return table
+M.same = function(node)
+  return f(function(args)
+    local text = args[1][1] or ""
+    return text
+  end, node)
+end
+
+---Inserts current time
+---@param format string
+---@return table
+M.time = function(format)
+  return f(function()
+    local date = vim.fn.strftime(format)
+    return date
+  end)
+end
+
+return M
